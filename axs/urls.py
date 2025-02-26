696f36cd4da9e351aca3b0630dfbd601025c1fab
alexsobreira99@hotmail.com <alexsobreira99@hotmail.com>
2025-02-26 18:43:05 -0300

    V - 26/02/2025 - 18:42

--- axs/urls.py
+++ axs/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import upload_docx
+from .views import convert_word_to_pdf, upload_docx
 
 urlpatterns = [
-    # path("convert-word-to-pdf/", convert_word_to_pdf, name="convert_word_to_pdf"),
+    path("convert-word-to-pdf/", convert_word_to_pdf, name="convert_word_to_pdf"),
      path("upload/", upload_docx, name="upload_docx"),
 ]
